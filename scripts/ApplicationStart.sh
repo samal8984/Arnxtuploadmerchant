902f0b4860f56ee7340502982ef51f5bbc72038f
anil samal <rajuanil22@gmail.com>
2022-12-13 16:18:50 +0530

    appspec updated

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/src
+cd /home/ec2-user/server/src
 npm start
-pm2 start npm --name "uploading app" -- start
+pm2 start npm --name "arnxtuploader" -- start
 pm2 startup
 pm2 save
 pm2 restart all
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/
+cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum -y install nodejs npm
 
