5fffdb6437222726ca4ba974e16b2efa83b80807
anil samal <rajuanil22@gmail.com>
2022-12-14 15:08:27 +0530

    app start changed

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd App
-npm start
-pm2 start npm --name "arnxtuploader" -- start
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "arnxtuploader" --watch
 pm2 startup
 pm2 save
 pm2 restart all
