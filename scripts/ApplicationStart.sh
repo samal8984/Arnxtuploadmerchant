93a98b74ea6d1c240cc073b43b7216d4c49292eb
anil samal <rajuanil22@gmail.com>
2022-12-15 12:34:54 +0530

    appstart changed

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd App
-npm run build
-pm2 serve build/ 3000 --name "react-build" --spa
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "uploading" --watch
 pm2 startup
 pm2 save
 pm2 restart all
